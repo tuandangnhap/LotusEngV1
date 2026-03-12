23c138794efec553fc8748a725f7d54f7b338b28
phankimton <phantonxddt@gmail.com>
2026-03-12 22:32:31 +0700

    update login feature

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
 const express = require("express")
 const http = require("http")
-const {Server} = require("socket.io")
+const { Server } = require("socket.io")
 const bodyParser = require("body-parser")
+const session = require("express-session")
 
 const apiRoutes = require("./routes/api")
 const productRoutes = require("./routes/products")
@@ -13,24 +13,68 @@ const server = http.createServer(app)
 const io = new Server(server)
 
 app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+    secret: "shopee-secret",
+    resave: false,
+    saveUninitialized: true
+}))
+
 app.use(express.static("public"))
 
-app.use("/api", apiRoutes)
-app.use("/products", productRoutes)
-app.use("/orders", orderRoutes)
+/* ===== LOGIN PAGE ===== */
 
-io.on("connection",(socket)=>{
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/public/login.html")
+})
 
-socket.on("cli-command",(cmd)=>{
+app.post("/login", (req, res) => {
 
-socket.emit("cli-result","Executed: "+cmd)
+    const { username, password } = req.body
+
+    if (username === "admin" && password === "123456") {
+        req.session.user = username
+        return res.redirect("/")
+    }
+
+    res.send("Sai tài khoản hoặc mật khẩu")
+})
+
+app.get("/logout", (req, res) => {
+    req.session.destroy()
+    res.redirect("/login")
+})
+
+/* ===== LOGIN MIDDLEWARE ===== */
+
+function checkLogin(req, res, next) {
+
+    if (!req.session.user) {
+        return res.redirect("/login")
+    }
+
+    next()
+}
+
+/* ===== PROTECTED ROUTES ===== */
+
+app.use("/api", checkLogin, apiRoutes)
+app.use("/products", checkLogin, productRoutes)
+app.use("/orders", checkLogin, orderRoutes)
+
+io.on("connection", (socket) => {
+
+    socket.on("cli-command", (cmd) => {
+
+        socket.emit("cli-result", "Executed: " + cmd)
+
+    })
 
 })
 
-})
+server.listen(3000, () => {
 
-server.listen(3000,()=>{
-
-console.log("Shopee Seller Tool running on http://localhost:3000")
+    console.log("Shopee Seller Tool running on http://localhost:3000")
 
 })
