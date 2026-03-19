1e3d3e9d1f993bc2bbaa06f6ee6861a4e12554dc
phankimton <phantonxddt@gmail.com>
2026-03-19 08:41:27 +0700

    done

--- server.js
+++ server.js
@@ -316,7 +316,7 @@ app.get("/total_parts", (req, res) => {
 
     })
 
-    const size = 200  // mỗi part ~200 file
+    const size = 100  // mỗi part ~200 file
     const total = Math.ceil(tasks.length / size)
 
     res.json({ total })
@@ -327,7 +327,7 @@ app.get("/download_media_part", async (req, res) => {
     try {
 
         const part = parseInt(req.query.part) || 0
-        const size = 200
+        const size = 100
 
         const cache = JSON.parse(fs.readFileSync("cache.json"))
         const items = Object.values(cache)
