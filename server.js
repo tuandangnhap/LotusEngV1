5e082013b1a960ebceb8c1d5cd7329939cb2262b
unknown <you@example.com>
2026-06-19 00:44:59 +0700

    done

--- server.js
+++ server.js
@@ -514,7 +514,7 @@ app.get("/get_items", async (req, res) => {
                     sign: signature,
                     offset,
                     page_size,
-                    item_status: "SHOPEE_DELETE"
+                    item_status: "BANNED"
                 }
             }
         )
