c10e15f54e68cda2942e50a789a6f197421ef29e
unknown <you@example.com>
2026-06-26 16:32:51 +0700

    done

--- server.js
+++ server.js
@@ -514,7 +514,7 @@ app.get("/get_items", async (req, res) => {
                     sign: signature,
                     offset,
                     page_size,
-                    item_status: "NORMAL"
+                    item_status: "UNLIST"
                 }
             }
         )
